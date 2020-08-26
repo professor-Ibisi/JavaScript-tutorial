546de34aab5557c8372a5f31ed75b173891f0b95
professor-Ibisi <yinemichael2020@outlook.com>
2020-08-26 15:20:49 +0300

    index1.js

--- index.js
+++
@@ -1,8 +0,0 @@
-  //my first JavaScript code!
-  console.log('hello world');
-  console.log('Monkeys  wave  goodbye')
-  console.log("Patch  my boat  with  chewing  gum")
-  //declaring a variables
-  let name='elias';
-  console.log(name);
-  console.log(Math.max(2,4)) ;
