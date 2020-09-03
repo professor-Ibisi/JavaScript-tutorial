9e1e7cf4598b8001ad0e8ff3d0aff17b6fa3fee5
professor-Ibisi <yinemichael2020@outlook.com>
2020-09-03 11:54:36 +0300

    added exercise on the same file

--- exercise1.js
+++ exercise1.js
@@ -1,4 +1,26 @@
+// exercise practice 
 let numberSign ='';
 for (let i = 1; i <= 8; i++){
     console.log(numberSign +='#')
 }
+// arrays
+var  listOfNumbers = [2,3,5,7,11];
+console.log(listOfNumbers[2]);
+console.log(listOfNumbers[2-1]);
+// objects
+var day1 = {
+    squirrel: false,
+    events: ["work","touched tree","pizza","running","television"]
+
+};
+console.log(day1.squirrel);
+console.log(day1.wolf);
+ // array exercise 
+ function phi (table) {
+    return (table [3] * table [0] - table [2] * table [1]) /
+     Math.sqrt((table[2]+table[3]) *
+              (table[0] + table[1])*
+              (table[1] + table[3])*
+              (table[0] + table[2]));
+ }
+ console.log(phi([76, 9, 4, 1]));
